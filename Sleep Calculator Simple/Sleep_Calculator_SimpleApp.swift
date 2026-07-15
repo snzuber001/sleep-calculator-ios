b6fd454dd0f0d266766b6d9a126955bc30edd091
Sam <snzuber@pm.me>
2026-07-15 14:23:06 -0600

    Initial Commit

---
+++ Sleep Calculator Simple/Sleep_Calculator_SimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_Calculator_SimpleApp.swift
+//  Sleep Calculator Simple
+//
+//  Created by Sam on 2026-07-15.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_Calculator_SimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
